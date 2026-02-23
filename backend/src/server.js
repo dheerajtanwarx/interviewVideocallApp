cfd7e5b9a498b838c6cd0d788f12790c53cd0822
dheerajsaini01 <tanwardheeraj00111@gmail.com>
2026-02-23 15:25:18 +0530

    create a new branch and added some basic usecases like link the frontend and backend , make it ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,30 @@
 import express from 'express'
 import dotenv from "dotenv"
+import path from "path"
+import { fileURLToPath } from "url"
 dotenv.config()
 const app = express()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
  
 
-app.use('/', (req, res)=>{
-    res.send("hello dheeraj saini ")
+app.get('/health', (req, res)=>{
+    res.status(200).json("this is the health page")
 })
+
+app.get('/books', (req, res)=>{
+    res.status(200).json("this is the books app")
+})
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+
+    app.get("/{*any}", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log(`app is listen on port ${process.env.PORT}`)
