49a103c8be6e75227183cf41bc2a199ecbfaf1ed
dheerajsaini01 <tanwardheeraj00111@gmail.com>
2026-02-23 17:02:46 +0530

    fix some issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,10 +19,10 @@ app.get('/books', (req, res)=>{
 
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
     app.get("/{*any}", (req, res)=>{
-        res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"))
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
     })
 }
 
